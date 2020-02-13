f2584be0db3b0aa964dfc389efd55e48517fa671
lemisky <1478449280@qq.com>
2020-02-13 20:20:32 +0800

    接口相关

---
+++ src/demo05/MyDemo01.java
@@ -0,0 +1,14 @@
+package demo05;
+
+/**
+ * 关于接口
+ */
+public class MyDemo01 {
+    public static void main(String[] args) {
+        MyClass myClass=new MyClass();
+        myClass.m1();
+        myClass.m2();
+        System.out.println(MyInterface01.Interface_Const);
+        MyInterface01.m3();
+    }
+}

d16ef7d8fdd641c46d29d71e8f2dbc4f3b720ab9
lemisky <1478449280@qq.com>
2020-02-13 20:17:32 +0800

    Scanner工具类，输入相关

---
+++ src/demo01/MyScanner.java
@@ -0,0 +1,41 @@
+package demo01;
+
+
+import java.util.Scanner;
+
+/**
+ * @author foyou
+ *
+ * 关于导包：
+ * 1.同包内，类引用无需import导包
+ * 2.java.lang中，也无需导包
+ * 3.其他则需使用import导包
+
+ * Scanner 流扫描器，在指定的流中获取输入
+ * 1.流中的任何数据都是字符串
+ * 2.scanner中不同的方式是将字符串转换成不同的结果返回
+ */
+
+public class MyScanner {
+    public static void main(String[] args) {
+        //标准输入扫描器
+        Scanner sc=new Scanner(System.in);
+
+        //获取输入并输出
+        //获取输入 字符串
+        System.out.println("请输入一个字符串：");
+        System.out.println(sc.next());
+        //获取输入 int
+        System.out.println("请输入一个数字：");
+        System.out.println(sc.nextInt());
+        //获取输入 double
+        System.out.println("请输入一个double");
+        System.out.println(sc.nextDouble());
+        //获取输入的 一整行
+        System.out.println("请输入一行:");
+        //过滤掉多余 回车、\r等
+        sc.nextLine();
+        System.out.println(sc.nextLine());
+
+    }
+}
---
+++ src/demo01/MyScanner2.java
@@ -0,0 +1,25 @@
+package demo01;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * 从文件中扫描
+ */
+public class MyScanner2 {
+    public static void main(String[] args) throws Exception{
+        //获取资源文件路径
+        String path = MyScanner2.class.getClassLoader().getResource("demo01/f1.txt").getPath();
+        //创建资源File对象并实例化Scanner
+        Scanner sc = new Scanner(new File(path));
+        System.out.println(sc.next());
+        System.out.println(sc.nextInt());
+        System.out.println(sc.nextDouble());
+        //过滤掉 \r
+        //在Windows中，回车是\r\n
+        //next()方法接收到一个\n就会结束，剩下一个\r就会被nextLine()接收掉
+        //所以需要过滤一下
+        sc.nextLine();
+        System.out.println(sc.nextLine());
+    }
+}

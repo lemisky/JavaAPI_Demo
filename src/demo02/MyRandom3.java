0402a25c236d7f76c058e20850d295c13cc11221
lemisky <1478449280@qq.com>
2020-02-13 20:18:35 +0800

    Random工具类，随机数相关

---
+++ src/demo02/MyRandom3.java
@@ -0,0 +1,21 @@
+package demo02;
+
+import java.util.Random;
+
+public class MyRandom3 {
+    public static void main(String[] args) {
+
+        //带参数的构造函数
+        //用于保存随机数结果
+        Random random = new Random(111);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("\t" + random.nextInt(10000));
+        }
+        System.out.println();
+        random.setSeed(111);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("\t" + random.nextInt(10000));
+        }
+
+    }
+}

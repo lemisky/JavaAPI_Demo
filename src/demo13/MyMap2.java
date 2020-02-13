f7d2cb5dbe83424b88719b4481a6a5d7d89bec22
lemisky <1478449280@qq.com>
2020-02-13 22:05:06 +0800

    Map存储自定义数据类型

---
+++ src/demo13/MyMap2.java
@@ -0,0 +1,14 @@
+package demo13;
+
+/**
+ * HashMap 存储自定义数据类型
+ *  Map集合key唯一，所以
+ *  作为key的元素，必须重写equal方法和hashcode方法，
+ *  以保证key唯一，与HashSet相同
+ */
+public class MyMap2 {
+    public static void main(String[] args) {
+        //此处省略
+        //见demo11/MySet.java:31
+    }
+}

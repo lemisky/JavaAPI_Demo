3b996d176e68a589f48a9d9ccbfd04eaf7f165b9
lemisky <1478449280@qq.com>
2020-02-17 19:24:18 +0800

    。。。

--- src/demo23_注解/MyAnnotation.java
+++ src/demo23_注解/MyAnnotation.java
@@ -1,4 +1,18 @@
 package demo23_注解;
 
+/**
+ * @author foyou
+ * @version 1.1
+ * @since JDK 1.5
+ * @deprecated java注解学习，javadoc使用
+ */
 public class MyAnnotation {
+    /**
+     * Java文档小demo
+     * @param message
+     * @param size
+     */
+    public static void doc(String message,int size){
+
+    }
 }

ac4cfcae554a75f2af654d514528a067661a4076
lemisky <1478449280@qq.com>
2020-02-17 19:30:43 +0800

    javadoc 命令可以依旧注解生成 相关帮助文档

--- src/demo23_注解/MyAnnotation.java
+++ src/demo23_注解/MyAnnotation.java
@@ -1,18 +1,18 @@
-package demo23_娉ㄨВ;
+package demo23_注解;
 
 /**
  * @author foyou
  * @version 1.1
- * @since JDK 1.5
- * @deprecated java娉ㄨВ瀛︿範锛宩avadoc浣跨敤
+ * @deprecated java注解学习，javadoc使用
  */
 public class MyAnnotation {
     /**
-     * Java鏂囨。灏廳emo
+     * Java文档小demo
      * @param message
      * @param size
      */
     public static void doc(String message,int size){
-
+        //使用 javadoc 命令生成 使用注解标示信息 的帮助文档
+        //
     }
 }

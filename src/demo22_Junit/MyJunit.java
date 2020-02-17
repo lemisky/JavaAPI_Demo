3ad97dd1f132159613d1769d3de6b237705cfe0b
lemisky <1478449280@qq.com>
2020-02-17 19:15:41 +0800

    Junit 5.4

---
+++ src/demo22_Junit/MyJunit.java
@@ -0,0 +1,21 @@
+package demo22_Junit;
+
+import org.junit.jupiter.api.*;
+
+
+public class MyJunit {
+    @BeforeEach
+    public void init(){
+        System.out.println("开始方法 init");
+    }
+    @AfterEach
+    public void close(){
+        System.out.println("MyJunit.close");
+    }
+    @Test
+    public void testAdd() {
+        System.out.println("测试类");
+        int result=8-3;
+        Assertions.assertEquals(5,result);
+    }
+}
